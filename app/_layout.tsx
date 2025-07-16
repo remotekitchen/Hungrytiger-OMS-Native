beebc1aa8a41561173ce8cd5a077f1ca3597d13b
nazmul162001 <nazmulprime162001@gmail.com>
2025-07-16 11:00:59 +0600

    update codebase with filebase routing

---
+++ app/_layout.tsx
@@ -0,0 +1,40 @@
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import "react-native-reanimated";
+import Toast from "react-native-toast-message";
+import "../global.css";
+// Optional: basic NativeBase theme (can remove or customize later)
+const theme = extendTheme({});
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <NativeBaseProvider theme={theme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" translucent />
+        <Toast />
+      </ThemeProvider>
+    </NativeBaseProvider>
+  );
+}
